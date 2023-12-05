9e1b14be355bd239e96ea6c3ad2fe8de4bd8c4ff
Danilo Djokic <danilo.djokic97@gmail.com>
2023-12-05 18:43:11 +0100

    Finish day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{BufReader, BufRead}};
+use std::{fs::File, io::{BufReader, BufRead}, collections::HashMap};
 
 fn main() {
     let file = File::open("input.txt").expect("Cannot open file");
@@ -22,13 +22,41 @@ fn main() {
 }
 
 fn get_line_value(line: &str) -> u32 {
-    let numbers: Vec<char> = line
-        .chars()
-        .filter(|c| c.is_numeric())
-        .collect();
+    let numbers_spelled = HashMap::from([
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9')
+    ]);
 
-    let mut value = numbers.first().unwrap().to_string();
-    value.push(*numbers.last().unwrap());
+    let mut numbers: Vec<(char, usize)> = vec![];
 
-    value.parse::<u32>().unwrap()
+    for key in numbers_spelled.keys() {
+        let indicies = line.match_indices(key);
+
+        for idx in indicies {
+            numbers.push((*numbers_spelled.get(key).unwrap(), idx.0));
+        }
+    }
+
+    for (i,c) in line.chars().enumerate() {
+        if c.is_numeric() {
+            numbers.push((c,i));
+        }
+    }
+
+    numbers.sort_by(|a, b| a.1.cmp(&b.1));
+
+    let mut value = numbers.first().unwrap().0.to_string();
+    value.push(numbers.last().unwrap().0);
+    let value = value.parse::<u32>().unwrap();
+
+    println!("Line: {}, Numbers: {:?}, Value: {}", line, numbers, value);
+
+    value
 }
