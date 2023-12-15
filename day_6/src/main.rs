a281456ccdbc22888607a0f8f4e90a84f9189cdc
Danilo <ddjokic@boylesports.com>
2023-12-15 14:25:59 +0100

    Day 6 part 2

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,19 +1,22 @@
-use std::{fs::File, io::{BufReader, BufRead}};
+use std::{fs::File, io::{BufReader, BufRead, Lines}};
 
 #[derive(Debug)]
 struct Race {
-    pub id: usize,
-    pub time: u32,
-    pub dist: u32,
-    win_hold_times: Vec<u32>,
+    pub time: u64,
+    pub dist: u64,
+    win_hold_times: Vec<u64>,
 }
 
 impl Race {
-    fn new(id: usize) -> Self {
-        Self { id, time: 0, dist: 0, win_hold_times: vec![] }
+    fn new(time: u64, dist: u64) -> Self {
+        Self { 
+            time,
+            dist,
+            win_hold_times: vec![]
+        }
     }
 
-    fn add_win_hold_time(&mut self, hold_time: u32) {
+    fn add_win_hold_time(&mut self, hold_time: u64) {
         self.win_hold_times.push(hold_time);
     }
 
@@ -22,89 +25,54 @@ impl Race {
     }
 }
 
-enum RaceValue {
-    Time,
-    Distance
-}
-
-impl RaceValue {
-    fn get_value_idx(idx: usize) -> Self {
-        match idx {
-            0 => RaceValue::Time,
-            1 => RaceValue::Distance,
-            _ => panic!("Unexpected index: {}", idx)
-        }
-    }
-}
-
 fn main() {
     let file = File::open("input.txt").expect("Cannot open file");
     let reader = BufReader::new(file);
 
-    let mut races = parse_races(reader);
-    let margin = calc_margin(&mut races);
+    let mut race = parse_race(reader);
+    let wins = calc_margin(&mut race);
 
-    println!("Margin: {}", margin);
+    println!("Wins: {}", wins);
 }
 
-fn calc_margin(races: &mut Vec<Race>) -> usize {
-    let mut total_margin = 1;
-
-    for race in races.iter_mut() {
-        for hold_time in 1..race.time {
-            let remaining_time = race.time - hold_time;
-            let distance = calc_distance(hold_time, remaining_time);
-            
-            if distance > race.dist {
-                race.add_win_hold_time(hold_time);
-            }
+fn calc_margin(race: &mut Race) -> usize {
+    for hold_time in 1..race.time {
+        let remaining_time = race.time - hold_time;
+        let distance = calc_distance(hold_time, remaining_time);
+        
+        if distance > race.dist {
+            race.add_win_hold_time(hold_time);
         }
-
-        total_margin *= race.win_hold_times();
     }
 
-    total_margin
+    race.win_hold_times()
 }
 
-fn calc_distance(hold_time: u32, run_time: u32) -> u32 {
+fn calc_distance(hold_time: u64, run_time: u64) -> u64 {
     hold_time * run_time
 }
 
-fn parse_races(reader: BufReader<File>) -> Vec<Race> {
-   let mut races: Vec<Race> = vec![];
+fn parse_race(reader: BufReader<File>) -> Race {
+    let mut lines = reader.lines();
+    let time = parse_line(&mut lines);
+    let dist = parse_line(&mut lines);
 
-    for (value_idx, line) in reader.lines().enumerate() {
-        for (race_idx, value) in line
-            .unwrap()
-            .split_once(":")
-            .unwrap()
-            .1
-            .to_owned()
-            .split_whitespace()
-            .filter_map(|s| s.parse::<u32>().ok())
-            .enumerate()
-        {
-            match races.iter_mut().find(|r| r.id == race_idx) {
-                Some(race) => {
-                    match RaceValue::get_value_idx(value_idx) {
-                        RaceValue::Time => race.time = value,
-                        RaceValue::Distance => race.dist = value
-                    }
-                },
-                None => {
-                    let mut race = Race::new(race_idx);
+    Race::new(time, dist)
+}
 
-                    match RaceValue::get_value_idx(value_idx) {
-                        RaceValue::Time => race.time = value,
-                        RaceValue::Distance => race.dist = value
-                    }
-        
-                    races.push(race);
-                }
-            }
-            
-        }
-    }
+fn parse_line(lines: &mut Lines<BufReader<File>>) -> u64 {
+    let num_line: String = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split_once(":")
+        .unwrap()
+        .1
+        .chars()
+        .filter(|&c| !c.is_whitespace())
+        .collect();
 
-    races
+    println!("Num str: {}", num_line);
+
+    num_line.parse::<u64>().unwrap()
 }
