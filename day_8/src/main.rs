85fb75ddc80c5d976a3769ec4b1d3749f1056d38
Danilo Djokic <danilo.djokic97@gmail.com>
2023-12-17 14:44:59 +0100

    Day 8 part 2

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -1,72 +1,88 @@
 use std::{fs::File, io::{BufReader, BufRead, Lines}, collections::HashMap};
 
-#[derive(Debug, Clone)]
-struct Node {
-    pub left: String,
-    pub right: String,
-    pub next_z: Option<String>,
-    pub steps_to: Option<u32>
-}
-
-impl Node {
-    fn new(left: String, right: String) -> Self {
-        Self { left, right, next_z: None, steps_to: None }
-    }
-
-    fn set_next_z(&mut self, name: String, map: &HashMap<String, Node>, dir: &String) {
-        let mut steps_to: u32 = 0;
-        let mut next_z: String = name.clone();
-
-        for dir in dir.chars().cycle() {
-            if next_z.ends_with('Z') {
-                break;
-            }
-
-            steps_to += 1;
-            let val = map.get(&next_z).unwrap();
-
-            match dir {
-                'L' => next_z = val.left.clone(),
-                'R' => next_z = val.right.clone(),
-                _ => panic!("Not L or R")
-            }
-        }
-
-        println!("For: {}, found: {}", name, next_z);
-
-        self.next_z = Some(next_z);
-        self.steps_to = Some(steps_to);
-    }
-}
 
 fn main() {
-    let file = File::open("test.txt").expect("Cannot open file");
+    let file = File::open("input.txt").expect("Cannot open file");
     let reader = BufReader::new(file);
 
     let steps = parse_file(reader);
     println!("Steps: {}", steps);
 }
 
-fn parse_file(reader: BufReader<File>) -> u32 {
+fn parse_file(reader: BufReader<File>) -> u128 {
     let mut lines = reader.lines();
 
-    let dir = lines.next().unwrap().unwrap();
+    let directions = lines.next().unwrap().unwrap();
     lines.next();
 
-    let mut map = parse_map(lines);
-    let clone = map.clone();
+    let map = parse_map(lines);
 
-    for (key, val) in map.iter_mut() {
-        val.set_next_z(key.clone(), &clone, &dir);
+    let mut starts: Vec<(&String, u128)> = map.keys()
+        .filter(|k| k.ends_with('A'))
+        .map(|k| (k, 0))
+        .collect();
+
+    for start in starts.iter_mut() {
+        start.1 = find_next_z(start.0.clone(), &directions, &map);
     }
 
-    
+    println!("Starts: {:?}", starts);
 
-    0
+    lcm_c(starts.iter().map(|s| s.1).collect())
 }
 
-fn parse_map(lines: Lines<BufReader<File>>) -> HashMap<String, Node> {
-    let mut map: HashMap<String, Node> = HashMap::new();
+fn find_next_z(mut start: String, directions: &String, map: &HashMap<String, (String, String)>) -> u128 {
+    let mut steps = 0;
+
+    for dir in directions.chars().cycle() {
+        if start.ends_with('Z') {
+            return steps;
+        }
+
+        steps += 1;
+
+        let val = map.get(&start).unwrap();
+        
+        match dir {
+            'L' => start = val.0.clone(),
+            'R' => start = val.1.clone(),
+            _ => panic!("Not L or R")
+        }
+    }
+
+    panic!("Can't find Z for {start}")
+}
+
+fn lcm_c(col: Vec<u128>) -> u128 {
+    col.iter().fold(1, |acc, &x| lcm(acc, x))
+}
+
+fn lcm(first: u128, second: u128) -> u128 {
+    first * second / gcd(first, second)
+}
+
+fn gcd(first: u128, second: u128) -> u128 {
+    let mut max = first;
+    let mut min = second;
+    if min > max {
+        let val = max;
+        max = min;
+        min = val;
+    }
+
+    loop {
+        let res = max % min;
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
+}
+
+fn parse_map(lines: Lines<BufReader<File>>) -> HashMap<String, (String, String)> {
+    let mut map: HashMap<String, (String, String)> = HashMap::new();
 
     for line in lines {
         let line = line.unwrap();
@@ -81,8 +97,7 @@ fn parse_map(lines: Lines<BufReader<File>>) -> HashMap<String, Node> {
             .map(|s| s.trim())
             .collect();
 
-        let node = Node::new(edges[0].to_owned(), edges[1].to_owned());
-        map.insert(source.to_owned(), node);
+        map.insert(source.to_owned(), (edges[0].to_owned(), edges[1].to_owned()));
     }
 
     map
