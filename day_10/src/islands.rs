a038d4025fac3e1d3ff1802139f53888a62b37eb
Danilo Djokic <danilo.djokic97@gmail.com>
2023-12-22 14:23:14 +0100

    Start flood fill modification

--- day_10/src/islands.rs
+++ day_10/src/islands.rs
@@ -1,5 +1,6 @@
-use crate::pipe::{MapElement, Point};
+use crate::pipe::{MapElement, Point, MapDirection, PipeDir};
 use queues::*;
+use crate::pipe::ElementType;
 
 pub fn split_islands(map: &mut Vec<Vec<MapElement>>) -> Vec<Vec<Point>> {
     let mut islands = vec![];
@@ -14,6 +15,18 @@ pub fn split_islands(map: &mut Vec<Vec<MapElement>>) -> Vec<Vec<Point>> {
     }
 
     islands
+}
+
+fn add_to_island(map: &mut Vec<Vec<MapElement>>, island: &mut Vec<Point>, queue: &mut Queue<Point>, curr: Option<Point>) {
+    if let Some(curr) = curr {
+        let map_el = &mut map[curr.0][curr.1];
+    
+        if !map_el.processed {
+            map_el.processed = true;
+            island.push(map_el.point.clone());
+            queue.add(map_el.point.clone()).unwrap();
+        }
+    }
 }
 
 fn flood_fill_island(map: &mut Vec<Vec<MapElement>>, start: Point) -> Vec<Point> {
@@ -31,45 +44,164 @@ fn flood_fill_island(map: &mut Vec<Vec<MapElement>>, start: Point) -> Vec<Point>
         let current = queue.remove().unwrap();
 
         if current.0 != 0 {
-            let up = Point(current.0 - 1, current.1);
-
-            if !&map[up.0][up.1].processed && map[up.0][up.1].is_ground() {
-                map[up.0][up.1].processed = true;
-                island.push(map[up.0][up.1].point.clone());
-                queue.add(map[up.0][up.1].point.clone()).unwrap();
-            }
+            let next = get_next_in_island(map, MapDirection::Up, &current);
+            add_to_island(map, &mut island, &mut queue, next);
         }
 
         if current.0 != map_bounds.0 - 1 {
-            let down = Point(current.0 + 1, current.1);
-
-            if !&map[down.0][down.1].processed && map[down.0][down.1].is_ground() {
-                map[down.0][down.1].processed = true;
-                island.push(map[down.0][down.1].point.clone());
-                queue.add(map[down.0][down.1].point.clone()).unwrap();
-            }
+            let next = get_next_in_island(map, MapDirection::Down, &current);
+            add_to_island(map, &mut island, &mut queue, next);
         }
 
         if current.1 != 0 {
-            let left = Point(current.0, current.1 - 1);
-
-            if !&map[left.0][left.1].processed && map[left.0][left.1].is_ground() {
-                map[left.0][left.1].processed = true;
-                island.push(map[left.0][left.1].point.clone());
-                queue.add(map[left.0][left.1].point.clone()).unwrap();
-            }
+            let next = get_next_in_island(map, MapDirection::Left, &current);
+            add_to_island(map, &mut island, &mut queue, next);
         }
 
         if current.1 != map_bounds.1 - 1 {
-            let right = Point(current.0, current.1 + 1);
-
-            if !&map[right.0][right.1].processed && map[right.0][right.1].is_ground() {
-                map[right.0][right.1].processed = true;
-                island.push(map[right.0][right.1].point.clone());
-                queue.add(map[right.0][right.1].point.clone()).unwrap();
-            }
+            let next = get_next_in_island(map, MapDirection::Right, &current);
+            add_to_island(map, &mut island, &mut queue, next);
         }
     }
 
     island
+}
+
+fn get_next_in_island(map: &mut Vec<Vec<MapElement>>, dir: MapDirection, current: &Point) -> Option<Point> {
+    let bounds = (map.len() as isize, map[0].len() as isize);
+
+    let next = from_dir(dir, current, 0);
+    if next.0 < 0 || next.0 >= bounds.0 || next.1 < 0 || next.1 >= bounds.1 {
+        return None
+    }  
+    
+    let next = Point(next.0 as usize, next.1 as usize);
+    let next_cell = &map[next.0][next.1];
+
+    if next_cell.is_ground() {
+        return Some(next)
+    }
+
+    return match dir {
+        MapDirection::Up => {
+            if let Some(next_reachable) = next_reachable_squeeze_left(map, next, MapDirection::Up) {
+                Some(next_reachable);
+            }
+
+            if let Some(next_reachable) = next_reachable_squeeze_right(map, next, MapDirection::Up) {
+                Some(next_reachable);
+            }
+
+            None
+        },
+        MapDirection::Down => {
+            if let Some(next_reachable) = next_reachable_squeeze_left(map, next, MapDirection::Down) {
+                Some(next_reachable);
+            }
+
+            if let Some(next_reachable) = next_reachable_squeeze_right(map, next, MapDirection::Down) {
+                Some(next_reachable);
+            }
+
+            None 
+        },
+        MapDirection::Left => {
+            if let Some(next_reachable) = next_reachable_squeeze_top(map, next, MapDirection::Left) {
+                Some(next_reachable);
+            }
+
+            if let Some(next_reachable) = next_reachable_squeeze_bot(map, next, MapDirection::Left) {
+                Some(next_reachable);
+            }
+
+            None 
+        },
+        MapDirection::Right => {
+            if let Some(next_reachable) = next_reachable_squeeze_top(map, next, MapDirection::Right) {
+                Some(next_reachable);
+            }
+
+            if let Some(next_reachable) = next_reachable_squeeze_bot(map, next, MapDirection::Right) {
+                Some(next_reachable);
+            }
+
+            None 
+        }
+    };
+}
+
+
+/// Next 4 functions should do the same thing accept look for different pipe directions
+/// and check only their respective up/down or left/right bounds 
+///
+/// from the starting point, keep moving until bounds is found or ground is found
+/// while loop to update the start and check the end condition 
+///
+/// for top/down movement
+/// squeeze on | can happen for J and 7 on the left and L and F on the right
+/// squeeze on J and 7 can only happen on the right for |, L or F
+/// squeeze on L and F can only happen on the left for | J or 7
+/// 
+/// for left/right movement
+/// squueze on - can only happen for L or J on the top or F or 7 on the bot
+/// squueze on F and 7 can only happen on the top for -, L or J
+/// squueze on L and J can only happen on the bot for -, F or 7 
+
+fn next_reachable_squeeze_left(map: &mut Vec<Vec<MapElement>>, start: Point, dir: MapDirection) -> Option<Point> {
+    match dir {
+        MapDirection::Up => {},
+        MapDirection::Down => {},
+        _ => panic!("Moving left/right when trying to squueze top/bot")
+    }
+}
+
+fn next_reachable_squeeze_right(map: &mut Vec<Vec<MapElement>>, start: Point, dir: MapDirection) -> Option<Point> {
+    match dir {
+        MapDirection::Up => {},
+        MapDirection::Down => {},
+        _ => panic!("Moving left/right when trying to squueze top/bot")
+    }
+}
+
+fn next_reachable_squeeze_top(map: &mut Vec<Vec<MapElement>>, start: Point, dir: MapDirection) -> Option<Point> {
+    match dir {
+        MapDirection::Left => {},
+        MapDirection::Right => {},
+        _ => panic!("Moving top/bot when trying to squueze left/right")
+    }
+}
+
+fn next_reachable_squeeze_bot(map: &mut Vec<Vec<MapElement>>, start: Point, dir: MapDirection) -> Option<Point> {
+    match dir {
+        MapDirection::Left => {},
+        MapDirection::Right => {},
+        _ => panic!("Moving top/bot when trying to squueze left/right")
+    }
+}
+
+fn has_left(point: &Point) -> bool {
+    point.1 > 0
+}
+
+fn has_right(point: &Point, bounds: (usize, usize)) -> bool {
+    point.1 < bounds.0 - 1
+}
+
+fn has_top(point: &Point) -> bool {
+    point.0 > 0
+}
+
+fn has_bot(point: &Point, bounds: (usize, usize)) -> bool {
+    point.0 < bounds.1 - 1
+}
+
+fn from_dir(dir: MapDirection, current: &Point, mut offset: isize) -> (isize, isize) {
+    offset = 1 + offset;
+
+    match dir {
+        MapDirection::Up => (current.0 as isize - offset, current.1 as isize),
+        MapDirection::Down => (current.0 as isize + offset, current.1 as isize),
+        MapDirection::Left => (current.0 as isize, current.1 as isize - offset),
+        MapDirection::Right => (current.0 as isize, current.1 as isize + offset)
+    }
 }
